28a2bac88b5ddc64e2dc17cf205e285bc510fc43
CasChrist <sozurukovmaksim@gmail.com>
2025-04-19 13:37:17 +0700

    Add API for User Deletion

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -17,6 +17,32 @@ const getProfile = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
+const deleteUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const userId = req.id;
+
+    if (!userId) {
+      res.status(400).json({ message: 'User ID is required' });
+      return;
+    }
+
+    const deletedUser = await UserModel.findByIdAndDelete(userId);
+
+    if (!deletedUser) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
+
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    res
+      .status(500)
+      .json({ error: (error as Error).message || 'Internal Server Error' });
+  }
+};
+
 export const userController = {
   getProfile,
+  deleteUser,
 };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,5 +5,6 @@ import { authenticateJWT } from '../middlewares/authMiddleware';
 const router = Router();
 
 router.get('/myProfile', authenticateJWT, userController.getProfile);
+router.delete('/myProfile', authenticateJWT, userController.deleteUser);
 
 export const userRoutes = router;
